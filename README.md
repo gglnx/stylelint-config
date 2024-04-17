7f6d99066effc064611fb1805a26b06b16a5f4ab
Dennis Morhardt <info@dennismorhardt.de>
2024-04-17 15:14:24 +0200

    🎉 Init project

---
+++ README.md
@@ -0,0 +1,48 @@
+# @gglnx/stylelint-config
+
+This is my personal Stylelint shared configuration. It's based on the [`stylelint-config-sass-guidelines`](https://github.com/bjankord/stylelint-config-sass-guidelines).
+
+## Installation
+
+Install the configuration and all peer dependencies:
+
+```bash
+npm install --save-dev @gglnx/stylelint-config
+```
+
+## Usage
+
+Create an `.stylelintrc` file in your project root:
+
+```json
+{
+  "extends": "@gglnx/stylelint-config"
+}
+```
+
+## Deviations
+
+* `csstools/use-logical`: Use logical props (except for float)
+* `selector-max-compound-selectors`: Limited to 4 compound selectors
+* `selector-class-pattern`: Allows only the BEM naming scheme (`.block__element--modifier`) and helper and theming classes (`.has-*`, `.u-*`, `.js-*`, `.is-*`, `.t-*`)
+* `max-nesting-depth`: Limits to one nesting level except for pseudo-classes, at-rules and theming classes (`.t-*`)
+* `property-no-unknown`: Adds allowance for `:export` property (CSS modules)
+* `order/properties-alphabetical-order`: Enforce alphabetical order for all properties
+* `order/order`: Custom order inside selectors:
+  1. SCSS variables
+  2. `@extend` at-rules
+  3. `@include use-*` at-rules
+  4. Custom properties
+  5. Declarations
+  6. Nested rules beginning with `&`
+  7. Nested rules for pseudo-classes (like `:hover`)
+  8. Nested rules for pseudo-elements (`::before/::after`)
+  9. Other rules and at-rules
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+Distributed under the [MIT License](https://opensource.org/license/mit). See `LICENSE` for more information.
---
+++ index.mjs
@@ -0,0 +1,83 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: 'stylelint-config-sass-guidelines',
+  plugins: [
+    'stylelint-order',
+    'stylelint-use-logical',
+    'stylelint-scss',
+  ],
+  rules: {
+    'csstools/use-logical': [true, {
+      except: ['float'],
+    }],
+    'selector-max-compound-selectors': 4,
+    // eslint-disable-next-line max-len
+    'selector-class-pattern': ['^(?:(?:(?<type>has|u|js|is)-(?<util>(?!(has|u|js|is|t)-+)(?:[a-z]|(?<!-)-(?!-))+))|(?:t-(?<theme>(?!t-+)(?:[a-z0-9]|(?<!-)-(?!-))+))|(?<block>(?!(has|u|js|t)-+)(?:[a-z]|(?<!-)-(?!-))+)(?:__(?<element>(?:[a-z]|(?<!-)-(?!-))+))?(?:--(?<modifier>(?:[a-z0-9]|(?<!-)-(?!-))+))?)$', {
+      resolveNestedSelectors: true,
+      message: "Class pattern \"%s\" doesn't match BEM rules",
+    }],
+    'max-nesting-depth': [1, {
+      ignore: ['blockless-at-rules', 'pseudo-classes'],
+      ignoreAtRules: ['media', 'each', 'if', 'include', 'supports', 'container'],
+      ignoreRules: ['/^.t-.+$/', '/::before/', '/::after/'],
+    }],
+    'property-no-unknown': [true, {
+      ignoreSelectors: [':export'],
+    }],
+    'order/properties-alphabetical-order': true,
+    'order/order': [
+      [
+        'dollar-variables',
+        {
+          type: 'at-rule',
+          name: 'extend',
+        },
+        {
+          type: 'at-rule',
+          name: 'include',
+          parameter: '^use-.*',
+        },
+        'custom-properties',
+        'declarations',
+        {
+          type: 'rule',
+          selector: '&$',
+        },
+        {
+          type: 'rule',
+          selector: '&:[\\w-]+|&\\[(.*?)\\]',
+        },
+        {
+          type: 'rule',
+          selector: '&::[\\w-]+',
+        },
+        'rules',
+        {
+          type: 'at-rule',
+        },
+        {
+          type: 'at-rule',
+          name: 'include',
+          parameter: 'mq',
+          hasBlock: true,
+        },
+        {
+          type: 'at-rule',
+          name: 'container',
+          hasBlock: true,
+        },
+        {
+          type: 'at-rule',
+          name: 'media',
+          hasBlock: true,
+        },
+      ],
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.scss', '**/*.scss'],
+      customSyntax: 'postcss-scss',
+    },
+  ],
+};
